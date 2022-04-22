62eba9d08108c44c4b1b18623c1f1cf746337746
nddruu <nddru@mail.ru>
2022-04-22 21:43:41 +0700

    Домашнее задание к уроку 4

--- homework_4/task_1.py
+++ homework_4/task_1.py
@@ -5,8 +5,26 @@
 
 from sys import argv
 
-script_name, output_in_hours, rate_per_hour, premium = argv
+def my_args(args):
+    try:
+        script_name, output_in_hours, rate_per_hour, premium = args
+        try:
+            return dict(output_in_hours = float(output_in_hours), rate_per_hour = float(rate_per_hour),
+                 premium = float(premium))
+        except ValueError as err:
+            return "Переданы не правильные параметры! Параметры должны быть числовыми!"
+    except ValueError as err:
+        if str(err).find("not enough values to unpack") != -1:
+           return "Передано слишком мало параметров!"
+        elif str(err).find("too many values to unpack") != 1:
+            return "Передано слишком много параметров!"
+        else:
+            return "При выполнении произошла ошибка:" + str(err)
 
-salary = int(output_in_hours) * int(rate_per_hour) + int(premium)
 
-print(f"Зарплата сотрудника: {output_in_hours} ч * {rate_per_hour} руб + {premium} руб = {salary} руб")
+dict_param = my_args(argv)
+if isinstance(dict_param, dict):
+    salary = dict_param["output_in_hours"] * dict_param["rate_per_hour"] + dict_param["premium"]
+    print(f"Зарплата сотрудника: {dict_param['output_in_hours']:0.2f} ч * {dict_param['rate_per_hour']:0.2f} руб + {dict_param['premium']:0.2f} руб = {salary:0.2f} руб")
+else:
+    print(dict_param)
